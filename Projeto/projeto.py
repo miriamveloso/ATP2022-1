60265864e484e05b956395805f0f649505996530
A100677 <114107243+clarasofiacunha@users.noreply.github.com>
2022-12-31 18:12:34 +0000

    Update projeto.py

--- Projeto/projeto.py
+++ Projeto/projeto.py
@@ -1,9 +1,61 @@
 import json
+import matplotlib.pyplot as plt
 
-def lerficheiro(fnome):
+#Carregar a BD 
+def carregarficheiro(fnome):
+    bd = []
     f = open(fnome, encoding="UTF-8")
-    bd = json.load(f)
+    bd=json.load(f)
     return bd
+
+# Inserir um novo indivíduo na base de dados
+def inserir(nome, idade, sexo, cidade, distrito, BI, descrição, profissao, party_abbr, party_name, religiao, desportos, 
+            animais, figura_publica_pt, marca_carro, destinos_favoritos, fumador, gosta_cinema, gosta_viajar, acorda_cedo,
+            gosta_ler, gosta_musica, gosta_comer, gosta_animais_estimacao, gosta_dancar, comida_favorita):
+
+    insere = []
+    pessoa={}
+    pessoa['nome']=nome
+    pessoa['idade']=idade
+    pessoa['sexo']=sexo
+
+    morada = {}
+    morada['cidade'] = cidade
+    morada['distrito'] = distrito
+    pessoa['morada'] = morada
+
+    pessoa['BI']=BI
+    pessoa['descrição']=descrição
+    pessoa['profissao']=profissao
+
+    partido_politico = {}
+    partido_politico['party_abbr'] = party_abbr
+    partido_politico['party_name'] = party_name
+    pessoa['partido_politico']=partido_politico 
+
+    pessoa['religiao']=religiao
+    pessoa['desportos']=[desportos]
+    pessoa['animais']=[animais] 
+    pessoa['figura_publica_pt']=figura_publica_pt
+    pessoa['marca_carro']=marca_carro
+    pessoa['destinos_favoritos']=[destinos_favoritos]
+
+    atributos = {}
+    atributos['fumador'] = fumador
+    atributos['gosta_cinema'] = gosta_cinema
+    atributos['gosta_viajar'] = gosta_viajar
+    atributos['acorda_cedo'] = acorda_cedo
+    atributos['gosta_ler'] = gosta_ler
+    atributos['gosta_musica'] = gosta_musica
+    atributos['gosta_comer'] = gosta_comer
+    atributos['gosta_animais_estimacao'] = gosta_animais_estimacao
+    atributos['gosta_dancar'] = gosta_dancar
+    atributos['comida_favortita'] = comida_favorita
+    pessoa['atributos'] = atributos
+    
+    pessoa['atributos']=atributos
+    insere.append(pessoa)
+    return insere
 
 
 # Consultar um indivíduo na base de dados: a partir do CC/BI ou a partir do nome
@@ -69,3 +121,14 @@ def top10(distrib):
     valores.sort(key = ordenaValores)
     novaDistrib = dict(valores[-10:])
     return novaDistrib
+
+# Gráfico de distribuições
+def plotdistrib(distrib, tipo): #tipo -> str
+    fig=plt.figure(figsize=(20,7))
+    plt.xlabel(tipo)
+    plt.ylabel("Número de indivíduos")
+    plt.bar(distrib.keys(), distrib.values(), color='green', width=0.3)
+    plt.xticks([x for x in range(0, len(distrib.keys()))], distrib.keys())
+    plt.title("Distribuição por " + tipo)
+    plt.show()
+    return
